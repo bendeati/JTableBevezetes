7e5252ccf36e86708654122b5e8e257a30457685
Bende Attila <bendeati@gmail.com>
2021-01-20 10:07:21 +0100

    .

---
+++ src/csomag/Mozi.java
@@ -0,0 +1,29 @@
+
+package csomag;
+
+
+public class Mozi {
+    private String cim;
+    private int ev;
+    private int nezoSzam;
+
+    public String getCim() {
+        return cim;
+    }
+
+    public int getEv() {
+        return ev;
+    }
+
+    public int getNezoSzam() {
+        return nezoSzam;
+    }
+
+    public Mozi(String cim, int ev, int nezoSzam) {
+        this.cim = cim;
+        this.ev = ev;
+        this.nezoSzam = nezoSzam;
+    }
+    
+    
+}
